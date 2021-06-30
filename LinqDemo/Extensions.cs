c2e73811b55860c6292c34a72e808507f77cc03b
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-06-30 13:00:35 +0200

    Add project files.

---
+++ LinqDemo/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+   public static class Extensions
+    {
+        public static string Add(this string str, string newWord)
+        {
+            return $"{str} {newWord}";
+        }
+
+        public static string Add(this string str, params string[] words)
+        {
+            return $"{str} {String.Join(" ", words)}";
+        }
+    }
+}
---
+++ LinqDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = GetPersons();
+            var numbersArray = Enumerable.Range(1, 10).ToArray();
+            var numbersList = Enumerable.Range(1, 10).ToList();
+        }
+
+        private static IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>
+                {
+                new("Nisse", 20),
+                new("Nisse", 21),
+                new("Nisse", 22),
+                new("Nisse", 23),
+                new("Nisse", 24),
+                new("Nisse", 24),
+                new("Nisse", 26),
+                new("Pelle" ,65),
+                new("Pelle" ,60),
+                new("Pelle" ,62),
+                new("Olle" , 66),
+                new("Sara" , 54),
+                new("Ida" ,  36),
+                new("Fia",   45),
+                new("Sophia-Magdalena", 32),
+            };
+        }
+    }
+}
